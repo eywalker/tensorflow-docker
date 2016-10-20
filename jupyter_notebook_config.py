6bf121b4ff057302d4b9e2402ca3cf426b3d415d
Edgar Y. Walker <edgar.walker@gmail.com>
2016-10-20 08:59:24 -0500

    Remove jupyter notebook related files

--- jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-# Accept all incoming requests
-c.NotebookApp.ip = '*'
-
-c.NotebookApp.port = 8888
-
-c.NotebookApp.open_browser = False
-
-c.MultiKernelManager.default_kernel_name = 'python3'
--- run_tensorflow.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir /tensorboard
-tensorboard --logdir=/tensorboard/ &
-jupyter notebook "$@"
